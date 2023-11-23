dedca371a1ccf09e9523baa365d9307ffea44b52
kangmin01 <minheek99@gmail.com>
2023-11-23 23:37:14 +0900

    팔로잉바 구현

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,6 +1,15 @@
+// 세션 상에서 얻을 수 있는 사용자 정보
 export type User = {
   name: string;
   username: string;
   email: string;
   image?: string;
+};
+
+export type SimpleUser = Pick<User, "username" | "image">;
+
+export type DetailUser = User & {
+  following: SimpleUser[];
+  followers: SimpleUser[];
+  bookmarks: String[];
 };
