86fd8d62c5e072cccf00bdbdcd548c2a8cf108d8
kangmin01 <minheek99@gmail.com>
2023-11-28 02:19:25 +0900

    유저 프로필

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,20 +1,24 @@
 // 세션 상에서 얻을 수 있는 사용자 정보
-export type User = {
+export type AuthUser = {
   name: string;
   username: string;
   email: string;
   image?: string;
 };
 
-export type SimpleUser = Pick<User, "username" | "image">;
+export type SimpleUser = Pick<AuthUser, "username" | "image">;
 
-export type DetailUser = User & {
+export type HomeUser = AuthUser & {
   following: SimpleUser[];
   followers: SimpleUser[];
   bookmarks: String[];
 };
 
-export type ProfileUser = User & {
+export type SearchUser = AuthUser & {
   following: number;
   followers: number;
+};
+
+export type ProfileUser = SearchUser & {
+  posts: number;
 };
