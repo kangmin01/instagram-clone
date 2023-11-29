37bee310847a71f623e57299c4922a64577af8a5
kangmin01 <minheek99@gmail.com>
2023-11-29 18:57:32 +0900

    set Route Segment Config: dynamic

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,8 @@
 import { searchUsers } from "@/app/service/user";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   return searchUsers().then((data) => NextResponse.json(data));
 }
