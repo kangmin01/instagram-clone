a2abe82117680a5eb92afe388619feb06ff3e531
kangmin01 <minheek99@gmail.com>
2023-12-03 23:14:20 +0900

    좋아요 기능 커스텀 훅 생성

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -7,7 +7,7 @@ import HeartFillIcon from "./ui/icons/HeartFillIcon";
 import BookmarkFillIcon from "./ui/icons/BookmarkFillIcon";
 import { SimplePost } from "@/model/post";
 import { useSession } from "next-auth/react";
-import { useSWRConfig } from "swr";
+import usePosts from "@/hooks/posts";
 
 type Props = {
   post: SimplePost;
@@ -20,13 +20,11 @@ export default function ActionBar({ post }: Props) {
   const liked = user ? likes.includes(user.username) : false;
   const [bookmarked, setBookmarked] = useState(false);
 
-  const { mutate } = useSWRConfig();
-
+  const { setLike } = usePosts();
   const handleLike = (like: boolean) => {
-    fetch("api/likes", {
-      method: "PUT",
-      body: JSON.stringify({ id, like }),
-    }).then(() => mutate("/api/posts"));
+    if (user) {
+      setLike(post, user.username, like);
+    }
   };
 
   return (
