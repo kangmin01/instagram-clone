7211c6206089eb18e33dbe3c298758801b925c7f
kangmin01 <minheek99@gmail.com>
2023-12-12 00:00:19 +0900

    코멘트: 홈UI

--- src/app/service/posts.ts
+++ src/app/service/posts.ts
@@ -108,7 +108,7 @@ export async function addComment(
   return client
     .patch(postId)
     .setIfMissing({ comments: [] })
-    .append("likes", [
+    .append("comments", [
       {
         comment,
         author: { _ref: userId, _type: "reference" },
