cbf645acaf84d89eb2f0aef25a6f9924b7e29e85
kangmin01 <minheek99@gmail.com>
2023-12-15 04:20:27 +0900

    새포스트 작성: 드래깅

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,19 @@
-export default function NewPage() {
-  return <p>new page</p>;
+import NewPost from "@/components/NewPost";
+import { authOptions } from "@/util/authOptions";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+};
+
+export default async function NewPage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    redirect("/auth/login");
+  }
+
+  return <NewPost user={session.user} />;
 }
